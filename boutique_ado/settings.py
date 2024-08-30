bc1dae31cdc486eb1d10a1ba06dc4d469fde0478
Gary Sorohan <garysorohan@gmail.com>
2024-08-30 11:35:40 +0000

    Update allowed hosts for heroku deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-soro82-boutiqueadov5-pxgg1vkyz95.ws.codeinstitute-ide.net', 'boutique-ado-soro.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-soro82-boutiqueadov5-pxgg1vkyz95.ws.codeinstitute-ide.net', 'boutique-ado-soro-cb75614490b7.herokuapp.com', 'localhost']
 
 
 # Application definition
